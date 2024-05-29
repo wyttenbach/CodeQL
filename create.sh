b3f6bd4854869991300fe52ad77a8c752892a7cb
Dale Wyttenbach <dale.wyttenbach@gmail.com>
2024-05-29 09:35:16 -0500

    untested

---
+++ create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [dbname]"
+    exit 1
+fi
+CODEQL_HOME=${CODEQL_HOME:=$TOOLS/codeql}
+CODEQL_DBS=${CODEQL_DBS:=$TOOLS/codeql-dbs}
+dbname="$1"
+$CODEQL_HOME/codeql database create "$CODEQL_DBS/$dbname" --language=java --build-mode
+exit $?
